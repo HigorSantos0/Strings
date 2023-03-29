fdfd384926d0d336bc05705f4f054a326928a973
Higor do Nascimento Santos <higor-santos123@hotmail.com>
2023-03-29 08:02:45 -0300

    Update substring.c

--- substring.c
+++ substring.c
@@ -2,62 +2,57 @@
 #include <string.h>
 
 /*
-Desenvolver uma função que, dada uma string
-s, crie uma substring que inicie na posição p de
+Desenvolver uma funÃ§Ã£o que, dada uma string
+s, crie uma substring que inicie na posiÃ§Ã£o p de
 s e contenha n caracteres.
-Observações: se p e/ou n forem inválidos, a
-substring será vazia; ii) se s não possuir n
-caracteres além de p, a substring a ser criada
-começará em p e terminará no final de s.
+ObservaÃ§Ãµes: se p e/ou n forem invÃ¡lidos, a
+substring serÃ¡ vazia; ii) se s nÃ£o possuir n
+caracteres alÃ©m de p, a substring a ser criada
+comeÃ§arÃ¡ em p e terminarÃ¡ no final de s.
 */
 
-void stringsub(char string1[], char substring[], int p);
+void stringsub(char string1[], char substring[], int p, int n);
 
 void main()
 {
 	char s[5], sub[5];
-	int pos, quant;
+	int pos, quant, n;
 	
 	printf("Entre com a string: ");
 	fflush(stdin);
 	gets(s);
 	
-	printf("Entre com a posicao q a string 2 ira começar: ");
+	printf("Entre com a posicao q a string 2 ira comeÃ§ar: ");
     scanf("%d", &pos);
+    
+    printf("Entre com a quantidade: ");
+    scanf("%d", &n);
+	
+	stringsub(s, sub, pos, n);
 	
 	printf("String1 %s \n", s);
 	printf("substring %s \n", sub);
 	
-	quant = strlen(s);
-	
-	if (pos < quant)
-	{
-		stringsub(s, sub, pos);
-	
-		printf("String1 %s \n", s);
-		printf("substring %s \n", sub);	
-	}
-	else
-		{
-			printf("String vazia!");
-			
-			printf("String1 %s \n", s);
-			printf("substring %s \n", sub);	
-		}
-	
 	
 }
-void stringsub(char string1[], char substring[], int p)
+void stringsub(char string1[], char substring[], int p, int n)
 {
 	int i, quant, j;
 	
 	quant = strlen(string1);
-
-	for(i = p, j = 0;i < quant;i++, j++)
-	{
-		substring[j] = string1[i];
-	}
 	
+	if((p > quant) || (p < 0) || (n > quant) || (n < 0))
+	{
+		strcpy(substring, "");
+	}
+	else
+		{
+			for(i = p, j = 0;(j < n) && (string1[i]);i++, j++)
+			{
+				substring[j] = string1[i];
+			}
+			substring[j] = '\0';		
+		}
 	
 }
 
